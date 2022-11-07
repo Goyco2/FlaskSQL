15ab2eb07804bbe24baabd8242a4ab23a7b22d53
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2022-11-07 08:03:20 +0000

    Verifica1

--- Verifica/app1.py
+++ Verifica/app1.py
@@ -15,23 +15,19 @@ import matplotlib.pyplot as plt
 conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='goycochea.gabriel', password='xxx123##', database='goycochea.gabriel')
 
 
-@app.route('/infoUser', methods=['GET'])
-def home():
-    return render_template('home.html')
+@app.route('/', methods=['GET'])
+def home1():
+    return render_template('home1.html')
 
-@app.route('/result', methods=['GET', 'POST'])
-def result():
-
-    Nome = request.args['Nome']
-    Cognome = request.args['Cognome']
-    query = f"select * from sales.customers where first_name = '{Nome}' and last_name = '{Cognome}' "
+@app.route('/result1', methods=['GET', 'POST'])
+def result1():
+    Nome = request.args['NomeS']
+    query = f"select staffs.first_name, staffs.last_name, stores.store_name from sales.staffs inner join sales.stores on sales.staffs.store_id = sales.stores.store_id where stores.store_name = '{Nome}' "
     d = pd.read_sql(query, conn)
-    if Nome not in d['first_name'] and Cognome not in d['last_name']:
-        return render_template('error.html')
+    if Nome not in d.store_name.tolist():
+        return render_template('errore1.html')
     else:
-        return render_template('result.html', nomiColonne = d.columns.values, dati = list(d.values.tolist()))
-        
-
+        return render_template('result1.html', nomiColonne = d.columns.values, dati = list(d.values.tolist()))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3245, debug=True)
+  app.run(host='0.0.0.0', port=3245, debug=True)
